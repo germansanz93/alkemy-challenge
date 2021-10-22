2a6178706592242f913c09ce4185ec0e9ff2d558
germansanz93 <germansanz93@gmail.com>
2021-10-22 09:17:20 -0300

    @mui/styles installed, Appbar.js done

--- client/alkemy-challenge-client/src/Appbar.js
+++ client/alkemy-challenge-client/src/Appbar.js
@@ -1,42 +1,44 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Button from '@mui/material/Button';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import CategoryIcon from '@mui/icons-material/Category';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Button from "@mui/material/Button";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import CategoryIcon from "@mui/icons-material/Category";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import withStyles from "@mui/styles/withStyles";
 
+import styles from "./styles/AppbarStyles";
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: `-${drawerWidth}px`,
     ...(open && {
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
@@ -46,34 +48,35 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
 );
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
+  justifyContent: "flex-end",
 }));
 
-export default function PersistentDrawerLeft() {
+function PersistentDrawerLeft(props) {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const {classes} = props;
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -84,32 +87,32 @@ export default function PersistentDrawerLeft() {
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open} style={{backgroundColor: '#683ab7'}}>
+      <AppBar position="fixed" open={open} style={{backgroundColor: "#683ab7"}}>
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
             Alkemy Challenge
           </Typography>
-          <Button color="inherit"><AccountCircleIcon fontSize='large'/></Button>
+          <Button color="inherit"><AccountCircleIcon fontSize="large"/></Button>
         </Toolbar>
       </AppBar>
       <Drawer
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            boxSizing: 'border-box',
+            boxSizing: "border-box",
           },
         }}
         variant="persistent"
@@ -117,41 +120,48 @@ export default function PersistentDrawerLeft() {
         open={open}
       >
         <DrawerHeader>
+          <div className={classes.leftUsrPanel}>
+            <AccountCircleIcon fontSize={"large"}/>
+            <div>
+              <Typography variant="h6" noWrap>User</Typography>
+              <Typography variant="h6" noWrap>Balance: $1234</Typography>
+            </div>
+          </div>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "ltr" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
         </DrawerHeader>
         <Divider />
         <List>
-          <ListItem button key='Dashboard'>
+          <ListItem button key="Dashboard">
             <ListItemIcon>
               <DashboardIcon />
             </ListItemIcon>
-            <ListItemText primary='Dashboard' />
+            <ListItemText primary="Dashboard" />
           </ListItem>
-          <ListItem button key='Movements'>
+          <ListItem button key="Movements">
             <ListItemIcon>
               <SwapHorizIcon />
             </ListItemIcon>
-            <ListItemText primary='Movements' />
+            <ListItemText primary="Movements" />
           </ListItem>
-          <ListItem button key='Incomes'>
+          <ListItem button key="Incomes">
             <ListItemIcon>
               <AddIcon />
             </ListItemIcon>
-            <ListItemText primary='Incomes' />
+            <ListItemText primary="Incomes" />
           </ListItem>
-          <ListItem button key='Expenses'>
+          <ListItem button key="Expenses">
             <ListItemIcon>
               <RemoveIcon />
             </ListItemIcon>
-            <ListItemText primary='Expenses' />
+            <ListItemText primary="Expenses" />
           </ListItem>
-          <ListItem button key='Categories'>
+          <ListItem button key="Categories">
             <ListItemIcon>
               <CategoryIcon />
             </ListItemIcon>
-            <ListItemText primary='Categories' />
+            <ListItemText primary="Categories" />
           </ListItem>
         </List>
       </Drawer>
@@ -188,3 +198,5 @@ export default function PersistentDrawerLeft() {
     </Box>
   );
 }
+
+export default withStyles(styles)(PersistentDrawerLeft);
---
+++ client/alkemy-challenge-client/src/styles/AppbarStyles.js
@@ -0,0 +1,6 @@
+export default {
+  leftUsrPanel: {
+    width: "100%",
+    margin: "14px auto",
+  },
+};
