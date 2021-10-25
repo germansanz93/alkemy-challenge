826ddc7d585ba1007fed504c9d78fb675a0919ce
germansanz93 <germansanz93@gmail.com>
2021-10-25 17:06:57 -0300

    Started with Recents component, seeded and some styles added

--- client/alkemy-challenge-client/src/Recents.js
+++ client/alkemy-challenge-client/src/Recents.js
@@ -3,80 +3,68 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 
-export default function AlignItemsList() {
+import styles from './styles/RecentsStyles';
+import withStyles from '@mui/styles/withStyles';
+
+function Recents(props) {
+  const { classes } = props;
+  const movements = [
+    {
+      date: '2021/10/10',
+      amount: 1200,
+      description: 'Salary',
+      category: 'Salary',
+      type: 1
+    },
+    {
+      date: '2021/10/25',
+      amount: 100.00,
+      description: 'Payment for rent',
+      category: 'Rent',
+      type: 2
+    },
+    {
+      date: '2021/10/26',
+      amount: 200.00,
+      description: 'Month Food',
+      category: 'Food',
+      type: 2
+    }
+  ]
   return (
-    <div className="listContainer">
+    <div className={classes.listContainer}>
       <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Brunch this weekend?"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Ali Connors
-                </Typography>
-                {" — I'll be in your neighborhood doing errands this…"}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Summer BBQ"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  to Scott, Alex, Jennifer
-                </Typography>
-                {" — Wish I could come, but I'm out of town this…"}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Oui Oui"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Sandra Adams
-                </Typography>
-                {' — Do you have Paris recommendations? Have you ever…'}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
+        {
+          movements.map(mov => (
+            <div>
+              <ListItem className={classes.ListItem} alignItems="flex-start">
+                <div className={mov.type == 1 ? classes.leftColorBarGreen : classes.leftColorBarRed}></div>
+                <ListItemText
+                  primary={`$ ${mov.amount} - Category: ${mov.category}`}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        sx={{ display: 'inline' }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {mov.date}
+                      </Typography>
+                      {` - ${mov.description}`}
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+              <Divider />
+            </div>
+          ))
+        }
       </List>
     </div>
   );
 }
+
+export default withStyles(styles)(Recents)
---
+++ client/alkemy-challenge-client/src/styles/RecentsStyles.js
@@ -0,0 +1,21 @@
+export default {
+  Listitem:{
+    position: 'relative',
+  },
+  leftColorBarGreen: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    bottom: '0',
+    width: '8px',
+    backgroundColor: 'green',
+  },
+  leftColorBarRed: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    bottom: '0',
+    width: '8px',
+    backgroundColor: 'red',
+  }
+};
