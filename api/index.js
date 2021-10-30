ba6cf4b858920c4e16f78c3ab32cdfb77b46a78c
germansanz93 <germansanz93@gmail.com>
2021-10-30 15:09:27 -0300

    Added api code to handle categories table data

--- api/index.js
+++ api/index.js
@@ -40,31 +40,38 @@ app.get('/', (req, res) => {
 //movements
 //get all movements
 app.get('/movements', async (req, res) => {
-  
+
   let query;
 
   //get movements using operation type and month filters
   if (req.query.type && req.query.month) {
     const { type, month } = req.query;
-    query = {text: 'SELECT * FROM movements WHERE EXTRACT(MONTH FROM mov_date) = $1 AND mov_type_id = $2',
-            values: [month, type]};
+    query = {
+      text: 'SELECT * FROM movements WHERE EXTRACT(MONTH FROM mov_date) = $1 AND mov_type_id = $2',
+      values: [month, type]
+    };
   }
   //get movements using operation type filter
-  else if(req.query.type){
+  else if (req.query.type) {
     const type = req.query.type;
-    query = {text: 'SELECT * FROM movements WHERE mov_type_id = $1',
-            values: [type]};
+    query = {
+      text: 'SELECT * FROM movements WHERE mov_type_id = $1',
+      values: [type]
+    };
   }
   //get movements using month filter
-  else if(req.query.month){
+  else if (req.query.month) {
     const month = req.query.month;
-    query = {text: 'SELECT * FROM movements WHERE EXTRACT(MONTH FROM mov_date) = $1',
-            values: [month]};
+    query = {
+      text: 'SELECT * FROM movements WHERE EXTRACT(MONTH FROM mov_date) = $1',
+      values: [month]
+    };
   }
   //if not using queries, get all movements
-  else
-  {
-    query = 'SELECT * FROM movements';
+  else {
+    query = 'SELECT movements.id, user_id, mov_date, mov_type_id, mov_description, category, amount FROM movements \
+    INNER JOIN categories \
+    ON mov_category_id = categories.id;'
   }
 
   //execute query
@@ -77,10 +84,15 @@ app.get('/movements', async (req, res) => {
 })
 //get last 10 movements
 app.get('/movements/recent', async (req, res) => {
+  const query = 'SELECT movements.id, user_id, mov_date, mov_type_id, mov_description, category, amount FROM movements \
+                INNER JOIN categories \
+                ON mov_category_id = categories.id \
+                ORDER BY mov_date DESC \
+                LIMIT 10;';
   try {
-    const recent_movements = await pool.query('SELECT * FROM movements ORDER BY mov_date DESC LIMIT 10');
+    const recent_movements = await pool.query(query);
     res.json(recent_movements.rows);
-  }catch (error) {
+  } catch (error) {
     console.error(error.message);
   }
 })
@@ -98,8 +110,8 @@ app.get('/movements/:id', async (req, res) => {
 app.post('/movements', async (req, res) => {
   try {
     const { id, date, type, description, amount } = req.body;
-    const newMovement = await pool.query('INSERT INTO movements (user_id, mov_date, mov_type_id, mov_description, amount) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [id, date, type, description, amount]);
+    const newMovement = await pool.query('INSERT INTO movements (user_id, mov_date, mov_type_id, mov_description, mov_category_id, amount) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      [id, date, type, description, category, amount]);
     res.json(newMovement.rows);
   } catch (error) {
     console.error(error.message);
@@ -120,8 +132,8 @@ app.put('/movements/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const { date, description, amount } = req.body;
-    const updateMovement = await pool.query('UPDATE movements SET mov_date = $1, mov_description = $2, amount = $3 WHERE id = $4 RETURNING *',
-      [date, description, amount, id]);
+    const updateMovement = await pool.query('UPDATE movements SET mov_date = $1, mov_description = $2, amount = $3, mov_category_id = $4, WHERE id = $5 RETURNING *',
+      [date, description, amount, category, id]);
     res.json(updateMovement.rows);
   } catch (error) {
     console.error(error.message);
